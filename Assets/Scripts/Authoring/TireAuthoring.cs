302c3487bae44ed263d92cb62565794d3e77a0b2
unforg1v3n <szymon.m992@gmail.com>
2024-07-27 19:13:38 +0200

    feat: Add dynamic(during baking) spring frequency and damping calculation using Jacobian Utilities. Minor parameters tweak.
    Truck prefab update

--- Assets/Scripts/Authoring/TireAuthoring.cs
+++ Assets/Scripts/Authoring/TireAuthoring.cs
@@ -3,6 +3,7 @@ using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Authoring;
 using AlphaTire.Components;
+using UnityEngine;
 
 namespace AlphaTire.Authoring
 {
@@ -10,10 +11,14 @@ namespace AlphaTire.Authoring
     {
         public float3 AnchorPosition;
         public float TargetDistance;
-        public float MaxImpulseAppliedByMotor = math.INFINITY;
 
-        public float SpringFrequency = Constraint.DefaultSpringFrequency;
-        public float SpringDampening = Constraint.DefaultDampingRatio;
+        public float MaxImpulseAppliedByMotor = math.INFINITY;
+        [Range(0.0f, 1.0f)]
+        public float SpringDampeningRatio = 0.7f;
+        [Range(0.0f, 1.0f)]
+        public float TauCosntraint = 0.6f;
+        [Range(2f, 20f)]
+        public int Iterations = 8;
 
         private float3 PositionInConnectedEntity;
         private float3 AxisInConnectedEntity;
@@ -56,9 +61,22 @@ namespace AlphaTire.Authoring
                 );
 
                 var constrains = joint.GetConstraints();
-                constrains[0] = Constraint.MotorPlanar(authoring.TargetDistance, math.abs(authoring.MaxImpulseAppliedByMotor), 
-                    math.abs(authoring.SpringFrequency), math.abs(authoring.SpringDampening));
+                JacobianCalculateSpringFrequencyAndDamping(authoring.TauCosntraint, authoring.SpringDampeningRatio, math.rcp(60.0f), authoring.Iterations, out var SpringFrequency, out var DampingRatio);
+                Debug.Log($"Calculated spring frequency: {SpringFrequency} damping: {DampingRatio}");
 
+                var motorPlanarConstraint = new Constraint
+                {
+                    ConstrainedAxes = new bool3(true, false, false),
+                    Type = ConstraintType.PositionMotor,
+                    Min = 0.0f,
+                    Max = 0.0f,
+                    SpringFrequency = SpringFrequency,
+                    DampingRatio = DampingRatio,
+                    MaxImpulse = new float3(authoring.MaxImpulseAppliedByMotor, 0f, 0f), 
+                    Target = new float3(authoring.TargetDistance, 0f, 0f),
+                };
+
+                constrains[0] = motorPlanarConstraint;
                 joint.SetConstraints(constrains);
                 joint.SetImpulseEventThresholdAllConstraints(authoring.MaxImpulse);
 
@@ -66,6 +84,26 @@ namespace AlphaTire.Authoring
                 var worldIndex = GetWorldIndexFromBaseJoint(authoring);
 
                 CreateJointEntity(worldIndex, constraintBodyPair, joint);
+            }
+
+            private void JacobianCalculateSpringFrequencyAndDamping(float constraintTau, float constraintDamping,
+            float timeStep, int iterations, out float springFrequency, out float dampingRatio)
+            {
+                int n = iterations;
+                float h = timeStep;
+                float hh = h * h;
+                float a = 1.0f - constraintDamping;
+                float aSum = 1.0f;
+
+                for (int i = 1; i < n; i++)
+                {
+                    aSum += math.pow(a, i);
+                }
+
+                float w = math.sqrt(constraintTau * aSum / math.pow(a, n)) / h;
+                float ww = w * w;
+                springFrequency = w / (2.0f * math.PI);
+                dampingRatio = (math.pow(a, -n) - 1 - hh * ww) / (2.0f * h * w);
             }
 
         }
